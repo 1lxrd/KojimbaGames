7c3ca5e4f60734227d48edfd88dbf526e879e0a8
Artem <lonedriver9x@gmail.com>
2020-05-08 19:01:29 +0300

    MainClass

---
+++ Assets/Scripts/main_core.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+ 
+namespace core_main
+{
+
+	class Person
+	{
+		public string name;
+		public int hp = 100;
+		public int mana = 100 ;
+	}
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person enemy = new Person;
+            Person user = new Pesron;
+
+        }
+    }
+}
