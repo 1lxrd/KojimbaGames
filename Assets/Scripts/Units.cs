70f7a425fb7e668cb92a023a811c61c28085bbf0
1lxrd <54149933+1lxrd@users.noreply.github.com>
2020-05-08 19:53:58 +0300

    Add files via upload

---
+++ Assets/Scripts/Units.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Units : MonoBehaviour
+{
+	public int hp = 100;
+	public int damage = 1;
+
+	/*public void Death()
+	{
+
+	}*/
+
+	public void DealDamage(object target)
+	{
+		target.hp -= damage;
+
+		if (target.hp <= 0)
+		{
+			target.death;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
